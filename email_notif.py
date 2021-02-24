72a989624cfc94ab5e015d5f8a861c090d4015df
Aakash <aakash@connect-172-31-36-182.vpn.tamu.edu>
2021-02-25 04:32:55 +0530

    modularized code

--- email_notif.py
+++ email_notif.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 from datetime import datetime
+import time
 import pickle
 import os.path
+import push
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -9,14 +11,9 @@ from google.auth.transport.requests import Request
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 CALENDAR_ID = 's53k5hnu5r6r68urvm9rkk5qp0@group.calendar.google.com'
+DAY_SECS = 86400
 
 def getCreds():
-
-
-def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -24,12 +21,15 @@ def main():
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
+    
+    return creds
+
+def getEvents():
+    creds = getCreds()
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
     now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
     
     events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=now,
                                         maxResults=10, singleEvents=True,
@@ -39,18 +39,31 @@ def main():
     now = datetime.now()
     print("NOW: ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
-    if not events:
-        print('No upcoming events found.')
-    for event in events[:1]:
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start[:-6], event['summary'])
+    return events
 
-        new_start = datetime.strptime(start[:-6], '%Y-%m-%dT%H:%M:%S')
-        print("NEW START: ", new_start)
-        print("DIFFERNCE: ", new_start - now)
+    # for event in events[:1]:
+    #     start = event['start'].get('dateTime', event['start'].get('date'))
+    #     print(start[:-6], event['summary'])
+
+    #     new_start = datetime.strptime(start[:-6], '%Y-%m-%dT%H:%M:%S')
+    #     print("NEW START: ", new_start)
+    #     print("DIFFERENCE: ", (new_start - now).seconds)
 
 def drive():
-    pass
+    while True:
+        events = getEvents()
+        
+        if not events:
+            time.sleep(DAY_SECS)
 
-if __name__ == '__main__':
-    main()
+        else:
+            for event in events:
+                print("EVENT SEEN")
+                now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                start_time = event['start'].get('dateTime', event['start'].get('date'))[:-6]
+                message = event['summary']
+                time.sleep((start_time - now).seconds - 600)
+                push.push(event['summray'] + " in 10 minutes!")
+
+print("STARTING NOW!")
+drive()
--- push.py
+++ push.py
@@ -1,5 +1,7 @@
 from notify_run import Notify
 notify = Notify()
 
+PUSH_CHANNEL = 'https://notify.run/jZbKmSjkEA0b7lQt'
 
-notify.send('Workout in 20 minutes', 'https://notify.run/jZbKmSjkEA0b7lQt')
+def push(message):
+    notify.send(message, PUSH_CHANNEL)
