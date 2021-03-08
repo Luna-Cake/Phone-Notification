f0b7c2c6c09187e24ba0a6261345549090094dc3
Aakash <aakash@Aakashs-iMac.local>
2021-03-09 02:51:57 +0530

    removed push file

--- email_notif.py
+++ email_notif.py
@@ -3,71 +3,84 @@ from datetime import datetime
 import time
 import pickle
 import os.path
-from push import push
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from notify_run import Notify
+import time
 
-# If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 CALENDAR_ID = 's53k5hnu5r6r68urvm9rkk5qp0@group.calendar.google.com'
 DAY_SECS = 86400
+notify = None
+PUSH_CHANNEL = None
+
+def authorize():
+    notify = Notify()
+    channel = notify.register()
+    PUSH_CHANNEL = channel.endpoint
+    
+    print(PUSH_CHANNEL)
+    time.sleep(30)
+    notify.send("HEROKU SUCCESS", PUSH_CHANNEL)
+    time.sleep(30)
+
+def push(message):
+    notify.send(message, PUSH_CHANNEL)
 
 def getCreds():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
     return creds
 
 def getEvents():
     creds = getCreds()
 
-    service = build('calendar', 'v3', credentials=creds)
 
+    service = build('calendar', 'v3', credentials=creds)
     now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     
     events_result = service.events().list(calendarId=CALENDAR_ID, timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
+    
     events = events_result.get('items', [])
-
-    now = datetime.now()
-    print("NOW: ", now.strftime("%Y-%m-%d %H:%M:%S"))
 
     return events
 
-    # for event in events[:1]:
-    #     start = event['start'].get('dateTime', event['start'].get('date'))
-    #     print(start[:-6], event['summary'])
+print("HERE")
+authorize()
+push.push("HEROKU NOTIFICATION SYSTEM STARTING NOW")
 
-    #     new_start = datetime.strptime(start[:-6], '%Y-%m-%dT%H:%M:%S')
-    #     print("NEW START: ", new_start)
-    #     print("DIFFERENCE: ", (new_start - now).seconds)
+while True:
+    events = getEvents()
+    
+    if not events:
+        time.sleep(DAY_SECS)
 
-def drive():
-    while True:
-        events = getEvents()
-        
-        if not events:
-            time.sleep(DAY_SECS)
+    else:
+        for event in events:
+            start_time = event['start'].get('dateTime', event['start'].get('date'))
+            new_start = datetime.strptime(start_time[:-6], '%Y-%m-%dT%H:%M:%S')
 
-        else:
-            for event in events:
-                print("EVENT SEEN")
-                start_time = event['start'].get('dateTime', event['start'].get('date'))
-                
-                new_start = datetime.strptime(start_time[:-6], '%Y-%m-%dT%H:%M:%S')
-                message = event['summary']
-                
-                print("SLEEPING FOR ", (new_start - datetime.now()).seconds - 600, " SECONDS")
+            print(event['summary'])
+            print((new_start - datetime.now()).seconds - 600,  "seconds")
+            time.sleep((new_start - datetime.now()).seconds - 600)
+            push.push(event['summary'] + " in 10 minutes!")
 
-                time.sleep((new_start - datetime.now()).seconds - 300)
-                push(event['summary'] + " in 10 minutes!")
+            time.sleep(300)
+            push.push(event['summary'] + " in 5 minutes!")
 
-print("STARTING NOW!")
-drive()
--- push.py
+++
@@ -1,7 +0,0 @@
-from notify_run import Notify
-notify = Notify()
-
-PUSH_CHANNEL = 'https://notify.run/jZbKmSjkEA0b7lQt'
-
-def push(message):
-    notify.send(message, PUSH_CHANNEL)
